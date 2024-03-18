deef05ff05a9f6abbd7613ce271ad00069bc8cf3
solomon-ctg <solomon.iroegbu@gmail.com>
2024-03-18 18:50:05 +0100

    Second Model, Get Parameters and Eloquent

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -12,8 +13,17 @@ class HomeController extends Controller
     {
         //$allCategories = ['Category 1', 'Category 2'];
         //$allCategories = DB::table('categories')->get();
-        $allCategories = Category::all();
-        return view('home', ['categories' => $allCategories]);
+        //$allCategories = Category::all();
+        $categories = Category::all();
+
+        //$posts = Post::orderBy('id', 'desc')->get();
+        //$posts = Post::latest()->get();
+        //$posts = Post::where('category_id', request('category_id'))->latest()->get();
+        $posts = Post::when(request('category_id'), function ($query) {
+            $query->where('category_id', request('category_id'));
+        })->latest()->get();
+        //return view('home', ['categories' => $allCategories]);
+        return view('home', compact('categories', 'posts'));
     }
 
 
