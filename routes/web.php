00e7a342aa6e71c549f2ebd11d847fa8ba42e7ea
solomon-ctg <solomon.iroegbu@gmail.com>
2024-03-19 00:18:23 +0100

    Route Parameters and Route Model Binding

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,16 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 
 
 //Route::view('/', 'home')->name('home');
 Route::get('/', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+//Route::get('posts/{postId}', [PostController::class, 'some_method']);
+Route::get('posts/{post}', [\App\Http\Controllers\PostController::class, 'show'])->name('post.show');
+
 Route::view('contact', 'contact')->name('contact');
 Route::view('about', 'about')->name('about');
 
