4a5b4c9e3292f42c7c3cd71e3e88914324458612
solomon-ctg <solomon.iroegbu@gmail.com>
2024-03-18 18:31:11 +0100

    MVC: Model, Controller and View with Foreach

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        //$allCategories = ['Category 1', 'Category 2'];
+        //$allCategories = DB::table('categories')->get();
+        $allCategories = Category::all();
+        return view('home', ['categories' => $allCategories]);
+    }
+
+
+}
