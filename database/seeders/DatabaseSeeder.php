0ae75133754d2d9e8205dd7a4410a58ffe360c08
solomon-ctg <solomon.iroegbu@gmail.com>
2024-03-20 12:08:04 +0100

    API: List of Data: Routing, Controllerers and clients Demo

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -21,5 +22,14 @@ class DatabaseSeeder extends Seeder
         ]);
 
         $this->call(AdminSeeder::class);
+
+
+    }
+
+
+    //Category
+    public function run(): void
+    {
+        Category::factory(10)->create();
     }
 }
