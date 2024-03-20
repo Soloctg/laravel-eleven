8a566fe93665969fd55b2dc086f4123b205122e9
solomon-ctg <solomon.iroegbu@gmail.com>
2024-03-20 13:45:15 +0100

    Geting Single Records and API Resources

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\CategoryResource;
 use App\Models\Category;
 use Illuminate\Http\Request;
 
@@ -11,6 +12,19 @@ class CategoryController extends Controller
     //
     public function index()
     {
-        return Category::all();
+        //return Category::all();
+        return CategoryResource::collection(Category::all());
+
+    }
+
+    public function show(Category $category)
+    {
+        //return $category;
+        return new CategoryResource($category);
+    }
+
+    public function list()
+    {
+        return CategoryResource::collection(Category::all());
     }
 }
